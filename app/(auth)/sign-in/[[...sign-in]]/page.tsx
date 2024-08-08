7d50d3168b1c20383ec21aa90c0dd2d8baba8db2
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-08 15:34:37 +0100

    setup sigin-in page

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,30 @@
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47] "> welcome back!</h1>
+          <p className="text-base text=[#7E8CA0]">
+            {" "}
+            Log in or create account to get back to your dashboard.
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden h-full items-center justify-center bg-blue-600  lg:flex">
+        <Image src="/logo.svg" alt="Finance logo" height={100} width={100} />
+      </div>
+    </div>
+  );
+}
