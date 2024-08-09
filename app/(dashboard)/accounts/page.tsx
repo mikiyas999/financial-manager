f07839879629c74acc7c896151e59545429e6cdd
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-09 15:16:06 +0100

    initial commit

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { client } from "@/lib/hono";
+import { z } from "zod";
 
 const Accounts = () => {
+  const account = client.api.accounts[":id"].$get({
+    param: {
+      id: "12345",
+    },
+  });
+
+  const accountss = client.api.accounts.$get();
   return <div>Accounts</div>;
 };
 
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,25 +1,28 @@
 import { Hono } from "hono";
 
 import { handle } from "hono/vercel";
-
+import accounts from "./accounts";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app
-  .get("hello", (c) => {
-    return c.json({
-      message: "hello world",
-    });
-  })
-  .get("/users", (c) => {
-    return c.text("users");
-  })
-  .get("/user/:id", (c) => {
-    const id = c.req.param("id");
-    const name = c.req.query("name");
-    return c.json({ "user id:": id, name: name });
-  });
+const routes = app.route("/accounts", accounts);
+// app
+//   .get("hello", (c) => {
+//     return c.json({
+//       message: "hello world",
+//     });
+//   })
+//   .get("/users", (c) => {
+//     return c.text("users");
+//   })
+//   .get("/user/:id", (c) => {
+//     const id = c.req.param("id");
+//     const name = c.req.query("name");
+//     return c.json({ "user id:": id, name: name });
+//   });
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
