f7e4b5696373f04eed47db2bc64b9963ca16d88e
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-10 20:24:59 +0100

    add sheet provider

---
+++ providers/sheet-provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useMountedState } from "react-use";
+
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+
+// import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+
+// import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+// import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+
+// import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
+// import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+
+export const SheetProvider = () => {
+  const isMounted = useMountedState();
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <EditAccountSheet />
+      <NewAccountSheet />
+
+      {/* <EditCategorySheet />
+      <NewCategorySheet />
+
+      <EditTransactionSheet />
+      <NewTransactionSheet /> */}
+    </>
+  );
+};
