9f03a1f31c291662e26bbdf12b9ea9cc9a219377
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-12 16:23:35 +0100

    initial commit

---
+++ features/categories/components/CategoryForm.tsx
@@ -0,0 +1,95 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { insertAccountSchema } from "@/db/schema";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+type CategoryFormProps = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const CategoryForm = ({
+  id,
+  defaultValues,
+  onSubmit,
+  onDelete,
+  disabled,
+}: CategoryFormProps) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values);
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        autoCapitalize="off"
+        autoComplete="off"
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          disabled={disabled}
+          render={({ field }) => (
+            <FormItem className="space-y-2">
+              <FormLabel>Name</FormLabel>
+
+              <FormControl>
+                <Input placeholder="Transportation, Food" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-full" disabled={disabled}>
+          {id ? "Save changes" : "Create category"}
+        </Button>
+
+        {!!id && (
+          <Button
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+            className="w-full"
+            variant="outline"
+          >
+            <Trash className="mr-2 size-4" />
+            Delete Category
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
---
+++ features/categories/components/edit-category-sheet.tsx
@@ -0,0 +1,99 @@
+import { Loader } from "lucide-react";
+import { z } from "zod";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { insertCategorySchema } from "@/db/schema";
+
+import { useConfirm } from "@/hooks/use-confirm-modal";
+
+import { CategoryForm } from "./CategoryForm";
+import { useOpenCategory } from "../hooks/use-open-category";
+import { useGetCategory } from "../api/use-get-category";
+import { useEditCategory } from "../api/use-edit-category";
+import { useDeleteCategory } from "../api/use-delete-category";
+
+const formSchema = insertCategorySchema.pick({
+  name: true,
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export const EditCategorySheet = () => {
+  const { isOpen, onClose, id } = useOpenCategory();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this Category."
+  );
+
+  const categoryQuery = useGetCategory(id);
+  const editMutation = useEditCategory(id);
+  const deleteMutation = useDeleteCategory(id);
+
+  const isPending = editMutation.isPending || deleteMutation.isPending;
+
+  const isLoading = categoryQuery.isLoading;
+
+  const onSubmit = (values: FormValues) => {
+    editMutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  const defaultValues = categoryQuery.data
+    ? {
+        name: categoryQuery.data.name,
+      }
+    : {
+        name: "",
+      };
+
+  const onDelete = async () => {
+    const ok = await confirm();
+
+    if (ok) {
+      deleteMutation.mutate(undefined, {
+        onSuccess: () => {
+          onClose();
+        },
+      });
+    }
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Sheet open={isOpen || isPending} onOpenChange={onClose}>
+        <SheetContent className="space-y-4">
+          <SheetHeader>
+            <SheetTitle>Edit Category</SheetTitle>
+
+            <SheetDescription>Edit an existing category.</SheetDescription>
+          </SheetHeader>
+
+          {isLoading ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Loader className="size-6  animate-spin text-muted-foreground" />
+            </div>
+          ) : (
+            <CategoryForm
+              id={id}
+              defaultValues={defaultValues}
+              onSubmit={onSubmit}
+              disabled={isPending}
+              onDelete={onDelete}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
