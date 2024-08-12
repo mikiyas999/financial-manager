3224a74482606d3c91e4551a358cea2ad1f77f32
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-12 16:22:28 +0100

    add category sheet

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -4,10 +4,8 @@ import { useMountedState } from "react-use";
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 
-// import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
-
-// import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
-// import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 
 // import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
 // import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
@@ -22,10 +20,10 @@ export const SheetProvider = () => {
       <EditAccountSheet />
       <NewAccountSheet />
 
-      {/* <EditCategorySheet />
+      <EditCategorySheet />
       <NewCategorySheet />
 
-      <EditTransactionSheet />
+      {/* <EditTransactionSheet />
       <NewTransactionSheet /> */}
     </>
   );
