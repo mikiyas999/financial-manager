bb9b93cc2bd83048e38221e6600f7a95a4139b6a
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-12 16:21:41 +0100

    add api endpoints

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,12 +2,17 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
+import categories from "./categories";
+import transaction from "./transactions";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-const routes = app.route("/accounts", accounts);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transaction", transaction);
 
 export const GET = handle(app);
 export const POST = handle(app);
