c26075c4cb0681205d6ed0969ab171da33b15270
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-13 19:37:00 +0100

    add transaction sheet

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -7,8 +7,8 @@ import { NewAccountSheet } from "@/features/accounts/components/new-account-shee
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 
-// import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
-// import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -23,8 +23,8 @@ export const SheetProvider = () => {
       <EditCategorySheet />
       <NewCategorySheet />
 
-      {/* <EditTransactionSheet />
-      <NewTransactionSheet /> */}
+      <EditTransactionSheet />
+      <NewTransactionSheet />
     </>
   );
 };
