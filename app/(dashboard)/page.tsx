8b6360b44e5ea9db635224b06968755acf708dc2
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-17 14:19:53 +0100

    add data grid and data chart

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { DataCharts } from "@/components/data-charts";
+import { DataGrid } from "@/components/data-grid";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="mx-auto -mt-6 w-full max-w-screen-2xl pb-10">
+      <DataGrid />
+
+      <DataCharts />
+    </div>
+  );
 };
 
 export default DashboardPage;
