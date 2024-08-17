e9fdc795541245170bc66a33238a2b203056820d
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-08-17 14:21:10 +0100

    write a seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -11,7 +11,7 @@ config({ path: ".env.local" });
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
 
-const SEED_USER_ID = "user_2gIujdiMTbMqEMKu0iUjgYUmox1";
+const SEED_USER_ID = "user_2kNVxnLA6E1FHaXKHZbvHnQ77OX";
 const SEED_CATEGORIES = [
   {
     id: "category_1",
